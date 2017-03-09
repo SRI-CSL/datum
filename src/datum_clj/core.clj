eb818036a0605c9005928a355ab6395ed9d788ea
Tim McCarthy <thoomfish@gmail.com>
2017-03-09 09:57:49 -0800

    Preliminary parser worker

--- src/datum_clj/core.clj
+++ src/datum_clj/core.clj
@@ -1,27 +1,26 @@
 (ns datum-clj.core
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]
-            [clojure.string :as str]))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
+            [clojure.string :as str]
+            [datum-clj.ops :as ops]))
 
 (def parser (insta/parser (clojure.java.io/resource "grammar.bnf")))
 
 (def test-file (slurp (io/file (io/resource "examples.txt"))))
 (def big-test-file (slurp (io/file (io/resource "6-evidence.txt"))))
+
 (defn first-line? [line]
   (.startsWith line "  ***"))
+
 (defn datum-line? [line]
   (.startsWith line "    ***"))
-(defn relevant-line [line]
-  (or (datum-line? line)
-      (first-line? line)))
+
+(def relevant-line?
+  (some-fn datum-line? first-line?))
+
 (defn relevant-lines [file]
-  (filter relevant-line (str/split-lines file)))
+  (filter relevant-line? (str/split-lines file)))
+
 (defn extract-datums [lines]
   (let [counter (atom 0)]
     (map (partial str/join "\n")
@@ -31,6 +30,7 @@
               (swap! counter inc))
             @counter)
           lines))))
+
 (defn parse-file [file]
   (pmap parser
         (extract-datums (relevant-lines file))))
@@ -49,3 +49,37 @@
     *** comment: new protein is acetylated on K320 [KAcAb]
     *** comment: new protein is phosed on S37 [phosAb]
     *** source: 9744860-Fig-7")
+
+(def origins
+  {"" "endogenous"
+   "x" "expressed"
+   "r" "recombinant"
+   "p" "purified"
+   "k" "knockin"
+   "s" "synthetic"})
+
+(defn make-point [letter1 nat letter2]
+  [:point (str letter1 nat letter2)])
+
+(defn make-mutation [[type text]]
+  {:mutation_type (name type)
+   :text text})
+
+(defn make-mutations [& muts]
+  (map make-mutation muts))
+
+(defn make-oprotein [[_ origin] protein [_ & mutations] [_ & modifications]]
+  {:origin (origins origin)
+   :protein protein
+   :mutations mutations
+   :modifications modifications})
+
+(def transform-rules
+  {
+   :point make-point
+   :mutations make-mutations
+   :oprotein make-oprotein
+   })
+
+(defn transform [ast]
+  (insta/transform transform-rules ast))
