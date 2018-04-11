30797cd9bf5bc1d1d1e496fc74a0480104ad6aec
Tim McCarthy <thoomfish@gmail.com>
2018-04-11 14:44:42 -0700

    Update Cheshire dependency

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/algo.generic "0.1.2"]
                  [instaparse "1.4.5"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [cheshire "5.7.1"]]
+                 [cheshire "5.8.0"]]
   :main ^:skip-aot com.sri.csl.datum.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
